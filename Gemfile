44981723f66668e2af7ccce0a9330d5ced174b23
Igor Alexandrov <igor.alexandrov@gmail.com>
2019-03-05 11:27:27 +0300

    Added dependencies and started benchmarks

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in faster_support.gemspec
 gemspec
+
+group :benchmarks do
+  gem 'benchmark-ips'
+  gem 'activesupport'
+end
+
---
+++ lib/faster_support/numbers.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'bigdecimal'
+
+module FasterSupport
+  class Numbers
+    def self.instance
+      @instance ||= new
+    end
+
+    def initialize
+      @zeros = {
+        BigDecimal => 0.to_d,
+        Rational => 0.to_r
+      }
+    end
+
+    def number_to_currency(amount, unit:)
+      str = case amount
+        when Integer    then integer_to_string(amount)
+        when Float      then float_to_string(amount)
+        when Rational   then rational_to_string(amount)
+        when BigDecimal then decimal_to_string(amount)
+        else raise ArgumentError.new("amount should be numeric")
+      end
+
+      min_idx = (amount < zero(amount.class)) ? 1 : 0
+      # 3 chars from the end to the dot and another 3 chars to the first delimeter position
+      idx = str.length - 6
+      while idx > min_idx
+        str.insert(idx, ','.freeze)
+        idx -= 3
+      end
+      str.insert(min_idx, ' '.freeze)
+      str.insert(min_idx, unit)
+      str
+    end
+
+    private
+
+    def integer_to_string(amount)
+      sprintf('%d.00'.freeze, amount)
+    end
+
+    # It works incorrect with float numbers
+    # that have more than 15 digits
+    # of its integer part.
+    # Float#round can return incorrect value.
+    def float_to_string(amount)
+      sprintf('%.2f'.freeze, amount.round(2))
+    end
+
+    # format BigDecimal as string to avoid a large string allocation
+    # implicit round call, b/c of inconsistency of rounding rules in Ruby
+    # https://bugs.ruby-lang.org/issues/12548
+    def decimal_to_string(amount)
+      str = amount.round(2).to_s
+      # put the second zero after . if it is missing
+      if str.rindex('.'.freeze) != str.length - 3
+        str.insert(-1, '0'.freeze)
+      end
+      str
+    end
+
+    def rational_to_string(amount)
+      int_part = amount.to_i
+      frac3 = (amount.numerator % amount.denominator)*1000 / amount.denominator
+      frac_part = frac3 / 10
+      last_digit = frac3 % 10
+      frac_part += 1 if last_digit >= 5
+      if frac_part == 100
+        int_part += 1
+        frac_part = 0
+      end
+      sprintf('%d.%02d'.freeze, int_part, frac_part)
+    end
+
+    def zero(klass)
+      @zeros[klass] || 0
+    end
+  end
+end
