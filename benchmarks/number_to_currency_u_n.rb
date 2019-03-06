d7dde2e12ce10d19995777814ebde9b14715f204
Igor Alexandrov <igor.alexandrov@gmail.com>
2019-03-06 13:13:32 +0300

    Update benchmarks

--- benchmarks/number_to_currency_u_n.rb
+++ benchmarks/number_to_currency_u_n.rb
@@ -2,34 +2,57 @@
 
 require_relative 'suite'
 
-puts 'ActiveSupport#number_to_currency VS FasterSupport#number_to_currency_u_n'.reverse_color
-puts "\nBigDecimal\n".green
+class NumberToCurrencyUNBenchmark
+  def run
+    puts 'ActiveSupport#number_to_currency VS FasterSupport#number_to_currency_u_n'.reverse_color
+    puts "\nBigDecimal\n".green
+    profile_time
+    results = profile_memory
+    print_memory_results(results)
+  end
 
-float = rand * 10**(rand(12)) - 50_000_000
-decimal = BigDecimal(float.to_s)
+  def number
+    return @number if defined?(@number)
 
-currency_unit = '$'
+    float = rand * 10**(rand(12)) - 50_000_000
+    @number = BigDecimal(float.to_s)
+  end
 
-Benchmark.ips do |x|
-  x.report('ActiveSupport') { ActiveSupport::NumberHelper::NumberToCurrencyConverter.convert(decimal, unit: currency_unit, format: '%u %n') }
-  x.report('FasterSupport') { FasterSupport::Numbers.number_to_currency_u_n(decimal, unit: currency_unit) }
+  def currency_unit
+    '$'
+  end
 
-  x.compare!
+  def profile_time
+    Benchmark.ips do |x|
+      x.report('ActiveSupport') { ActiveSupport::NumberHelper::NumberToCurrencyConverter.convert(number, unit: currency_unit, format: '%u %n') }
+      x.report('FasterSupport') { FasterSupport::Numbers.number_to_currency_u_n(number, unit: currency_unit) }
+
+      x.compare!
+    end
+  end
+
+  def profile_memory
+    as_result = MemoryProfiler.report do
+      100.times { ActiveSupport::NumberHelper::NumberToCurrencyConverter.convert(number, unit: currency_unit, format: '%u %n') }
+    end
+    fs_result = MemoryProfiler.report do
+      100.times { FasterSupport::Numbers.number_to_currency_u_n(number, unit: currency_unit) }
+    end
+
+    [as_result, fs_result]
+  end
+
+  def print_memory_results(results)
+    table = Terminal::Table.new do |t|
+      t.headings = ['Memory', 'Allocated', 'Retained']
+      t.rows = [
+        ['ActiveSupport', results[0].total_allocated_memsize, results[0].total_retained_memsize],
+        ['FasterSupport', results[1].total_allocated_memsize, results[1].total_retained_memsize]
+      ]
+    end
+
+    puts table
+  end
 end
 
-as_result = MemoryProfiler.report do
-  100.times { ActiveSupport::NumberHelper::NumberToCurrencyConverter.convert(decimal, unit: currency_unit, format: '%u %n') }
-end
-fs_result = MemoryProfiler.report do
-  100.times { FasterSupport::Numbers.number_to_currency_u_n(decimal, unit: currency_unit) }
-end
-
-table = Terminal::Table.new do |t|
-  t.headings = ['Memory', 'Allocated', 'Retained']
-  t.rows = [
-    ['ActiveSupport', as_result.total_allocated_memsize, as_result.total_retained_memsize],
-    ['FasterSupport', fs_result.total_allocated_memsize, fs_result.total_retained_memsize]
-  ]
-end
-
-puts table
+NumberToCurrencyUNBenchmark.new.run
